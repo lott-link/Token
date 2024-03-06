ca30c6519d70d87319e5c611c59dc4e5ee543f8c
Renope <r.nouripoor@gmail.com>
2024-03-06 16:41:01 +0330

    adding supplyReserve to keep space from cap

--- contracts/LottLinkToken.sol
+++ contracts/LottLinkToken.sol
@@ -7,22 +7,26 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "./extensions/ERC20Capped.sol";
 
 contract LottLink is ERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20Votes, ERC20Capped {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant SUPPLY_RESERVE_ROLE = keccak256("SUPPLY_RESERVE_ROLE");
 
-    constructor(address defaultAdmin, address minter)
+    constructor(address defaultAdmin)
         ERC20("Lott.Link", "LLTT")
         ERC20Permit("Lott.Link")
         ERC20Capped(7777777 ether)
     {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+    }
+
+    function reserveSupply(uint256 amount) public onlyRole(SUPPLY_RESERVE_ROLE) {
+        _reserveSupply(amount);
     }
 
     // The following functions are overrides required by Solidity.
